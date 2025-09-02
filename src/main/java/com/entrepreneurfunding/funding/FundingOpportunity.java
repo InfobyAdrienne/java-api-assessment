af9eef6f2fdff048ae58ff534ae4ef86920c10be
Adrienne Galloway <aasgalloway@gmail.com>
2025-09-02 19:11:12 +0100

    completing all getters and setters except updatedAt

--- src/main/java/com/entrepreneurfunding/funding/FundingOpportunity.java
+++ src/main/java/com/entrepreneurfunding/funding/FundingOpportunity.java
@@ -40,13 +40,66 @@ public class FundingOpportunity {
 
   public FundingOpportunity() {
     this(
-        "Unknown",      // provider
-        IndustryFocus.OTHER,     // industry focus
-        BigDecimal.ZERO,         // minimumAmount
-        BigDecimal.ZERO,         // maximumAmount
-        Instant.now(),           // updatedAt
-        FundingStatus.PENDING    // default status
+        "Unknown", // provider
+        IndustryFocus.OTHER, // industry focus
+        BigDecimal.ZERO, // minimumAmount
+        BigDecimal.ZERO, // maximumAmount
+        Instant.now(), // updatedAt
+        FundingStatus.PENDING // default status
     );
   }
+  
+  // Create getter and setter methods for each field, except id, which should only have a getter
+  public UUID getId() {
+    return id;
+  }
 
+  public String getProvider() {
+    return provider;
+  }
+
+  public void setProvider(String provider) {
+    this.provider = provider;
+  }
+
+  public IndustryFocus getIndustryFocus() {
+    return industryFocus;
+  }
+
+  public void setIndustryFocus(IndustryFocus industryFocus) {
+    this.industryFocus = industryFocus;
+  }
+
+  // get maximum amount 
+  public BigDecimal getMaximumAmount() {
+    return maximumAmount;
+  }
+
+  // set maximum amount 
+  public void setMaximumAmount(BigDecimal maximumAmount) {
+    this.maximumAmount = maximumAmount;
+  }
+  
+  // get minimumim amount 
+  public BigDecimal getMinimumAmount() {
+    return minimumAmount;
+  }
+
+  // set minimumim amount 
+  public void setMinimumAmount(BigDecimal minimumAmount) {
+    this.minimumAmount = minimumAmount;
+  }
+
+  // Updated at should be updated whever there is a change to the table, dont know how to do this? 
+
+
+  // get status 
+  public FundingStatus getFundingStatus() {
+    return fundingStatus;
+  }
+  // if status is not declared, return the default PENDING
+
+  public void setFundingStatus(FundingStatus fundingStatus) {
+    this.fundingStatus = fundingStatus;
+  }
 }
